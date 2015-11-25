856b31ff15558980ad0bf21760615c382496c29b
Zack Tillotson <zachery.tillotson@gmail.com>
2015-11-25 11:18:52 -0700

    Tutorial work - looking better but probably still needs work

--- src/components/draft/DraftTutorial/DraftTutorial.jsx
+++ src/components/draft/DraftTutorial/DraftTutorial.jsx
@@ -35,56 +35,103 @@ export default React.createClass({
         return '';
       case 1:
         return (
-          <div className="step">
+          <div className="step" onClick={this.nextStepClickHandler}>
             <div className="top step1">
             </div>
             <div className="bottom step1">
-              <h3>Stress free drafting made easy.</h3>
+              <h3>Stress free drafting, the way it should be.</h3>
               <div className="promoImage"></div>
+              <table className="highlights threeWide">
+                <tr>
+                  <td>
+                    Easily see who is available to draft.
+                  </td>
+                  <td>
+                    Quickly enter your choice.
+                  </td>
+                  <td>
+                    Automatically synchronizes data between all captains.
+                  </td>
+                </tr>
+              </table>
               <div className="navLink">
-                <span className="nextStep" onClick={this.nextStepClickHandler}>
-                  Learn More
-                </span>
+                Click to learn more
               </div>
             </div>
           </div>
         );
       case 2:
         return (
-          <div className="step">
+          <div className="step" onClick={this.nextStepClickHandler}>
             <div className="top step2">
             </div>
             <div className="bottom step2">
               <h3>The team draft order</h3>
+              <div className="promoImage"></div>
+              <table className="highlights twoWide">
+                <tr>
+                  <td>
+                    This area shows the order the teams draft in.
+                  </td>
+                  <td>
+                    Snake draft format (i.e., 1&#8594;10, 10&#8594;1, 1&#8594;10, etc.)
+                  </td>
+                </tr>
+              </table>
               <div className="navLink">
-                <span className="nextStep" onClick={this.nextStepClickHandler}>
-                  Learn More
-                </span>
+                Click to learn more
               </div>
             </div>
           </div>
         );
       case 3:
         return (
-          <div className="step">
+          <div className="step" onClick={this.nextStepClickHandler}>
             <div className="top step3">
             </div>
             <div className="bottom step3">
               <h3>Select your team.</h3>
+              <div className="promoImage"></div>
+              <table className="highlights threeWide">
+                <tr>
+                  <td>
+                    Click &apos;Select Team&apos; to see your draft options.
+                  </td>
+                  <td>
+                    View team statistics.
+                  </td>
+                  <td>
+                    Get reminded when it&apos;s your turn to draft.
+                  </td>
+                </tr>
+              </table>
               <div className="navLink">
-                <span className="nextStep" onClick={this.nextStepClickHandler}>
-                  Learn More
-                </span>
+                Click to learn more
               </div>
             </div>
           </div>
         );
       case 4:
         return (
-          <div className="step">
+          <div className="step" onClick={this.nextStepClickHandler}>
             <div className="top step4">
               <h3>Click on a player to draft them.</h3>
-              <div className="promoImage"></div>
+              <table className="highlights threeWide">
+                <tr>
+                  <td>
+                    Click a player to open the draft form.
+                  </td>
+                  <td>
+                    Use the form to review the player and their baggage.
+                  </td>
+                  <td>
+                     Submit the draft form to draft the player.
+                  </td>
+                </tr>
+              </table>
+              <div className="navLink">
+                Click to close tutorial
+              </div>
             </div>
             <div className="bottom step4">
             </div>
