e2df57f5555d0dd28cdd78e7d7eea2aa2a28a05a
Zack Tillotson <zachery.tillotson@gmail.com>
2016-01-12 16:15:35 -0700

    Fix gender limits bug

--- src/draft/selector/tier/index.js
+++ src/draft/selector/tier/index.js
@@ -1,5 +1,11 @@
 // Direct conversion from state
 
+import utils from '../../utils';
+
+function getBaggageId(playerId, players, columns) {
+  return utils.getBaggageId(players.find(player => utils.getPlayerId(player, columns) == playerId), columns);
+}
+
 import {createSelector} from 'reselect';
 
 const userIm = state => state.user;
@@ -13,11 +19,17 @@ const connection = createSelector(connectionIm, connection => connection.toJS())
 const players = createSelector(firebaseIm, firebaseIm => firebaseIm.players.toJS());
 const teams = createSelector(firebaseIm, firebaseIm => firebaseIm.teams.toJS().filter(team => !!team));
 const columns = createSelector(firebaseIm, firebaseIm => firebaseIm.columns.toJS());
-const drafts = createSelector(firebaseIm, firebaseIm => firebaseIm.drafts.toJS().filter(draft => !draft.type));
-const baggageDrafts = createSelector(firebaseIm, firebaseIm => firebaseIm.drafts.toJS().filter(draft => draft.type == 'baggage'));
 const draftMeta = createSelector(firebaseIm, firebaseIm => firebaseIm.draftMeta.toJS());
 const draft = createSelector(firebaseIm, firebaseIm => firebaseIm.draft.toJS());
 const auth = createSelector(firebaseIm, firebaseIm => firebaseIm.auth.toJS());
 const userData = createSelector(firebaseIm, firebaseIm => firebaseIm.userData.toJS());
+const drafts = createSelector(firebaseIm, firebaseIm => firebaseIm.drafts.toJS().filter(draft => !draft.type));
+const baggageDrafts = createSelector(firebaseIm, drafts, players, columns, (firebaseIm, drafts, players, columns) => {
+  return firebaseIm.drafts.toJS()
+    .filter(draft => draft.type == 'baggage')
+    .filter(baggageDraft => !drafts.find(realDraft => 
+      (realDraft.playerId == baggageDraft.playerId) || (realDraft.playerId == getBaggageId(baggageDraft.playerId, players, columns))
+    ));
+});
 
 export default {user, ui, players, teams, columns, drafts, baggageDrafts, connection, draft, draftMeta, auth, userData};
--- src/draft/selector/tier/tier/tier/tier/user.js
+++ src/draft/selector/tier/tier/tier/tier/user.js
@@ -6,37 +6,34 @@ export const genderDrafts = createSelector(contextTeam, drafts, baggageDrafts, p
   (contextTeam, drafts, baggageDrafts, players, columns) => {
     let male = 0, female = 0;
     if(contextTeam !== null) {
+      const teamIds = {};
       drafts
         .filter(draft => draft.teamId == contextTeam)
         .forEach(draft => {
           const player = players.find(player => utils.getPlayerId(player, columns) == draft.playerId);
-          if(utils.getGender(player, columns) == 'M') {
-            male++;
-          } else {
-            female++;
-          }
-          if(utils.getBaggageId(player, columns)) {
+          teamIds[draft.playerId] = utils.getGender(player, columns);
+          const baggageId = utils.getBaggageId(player, columns);
+          if(!!baggageId) {
             const baggage = players.find(
               baggage => utils.getPlayerId(baggage, columns) == utils.getBaggageId(player, columns)
             );
-            if(utils.getGender(baggage, columns) == 'M') {
-              male++;
-            } else {
-              female++;
-            } 
+            teamIds[baggageId] = utils.getGender(baggage, columns);
           }
         });
       baggageDrafts
         .filter(draft => draft.teamId == contextTeam)
-        .filter(draft => !drafts.find(regDraft => !regDraft.type && regDraft.playerId == draft.playerId))
         .forEach(draft => {
           const player = players.find(player => utils.getPlayerId(player, columns) == draft.playerId);
-          if(utils.getGender(player, columns) == 'M') {
-            male++;
-          } else {
-            female++;
-          }
+          teamIds[draft.playerId] = utils.getGender(player, columns);
         });
+      Object.keys(teamIds).forEach(id => {
+        if(teamIds[id] == 'M') {
+          male++;
+        } else {
+          female++;
+        }
+      });
+    }
+    return {male, female};
   }
-  return {male, female};
-});
+);
